305ef48637ff5bfcb42fa33ea3fefd20ec5da85f
a1ike <a1ike28@gmail.com>
2024-02-11 12:50:14 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Button} from "./components/Button"
+import { useCallback, useEffect, useRef, useState } from "react";
+import pitchAnalyser from "pitch-analyser";
+import "./App.css";
+import "./components/Background/index.css";
+import { Button } from "./components/Button";
+import { Background } from "./components/Background";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [currentNote, setCurrentNote] = useState("");
+
+  function handleStart() {
+    const analyser = new pitchAnalyser({
+      callback: function (payload) {
+        console.log(payload); // E.g. { frequency: 220, note: "A3" }
+      },
+    });
+    analyser.initAnalyser().then(() => {
+      // Start the analyser after initialisation
+      analyser.startAnalyser();
+    });
+  }
+
+  function handleStop() {
+    alert("ПОШЕЛ ТЫ");
+  }
 
   return (
     <>
-      <h1>What Note?</h1>
+      <Background />
+      <div className="flex flex-col min-h-screen justify-center items-center p-10">
+        <h1 className="text-white">What Note?</h1>
+        <Button onClick={handleStart}>Как дела</Button>
+        <Button onClick={handleStop}>STOP IT</Button>
+      </div>
     </>
-  )
-}
-
-export default App
+  );
+};
